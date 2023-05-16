6e3956ddfd03ab6d51d38b8f895c00103cad1747
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-05-16 09:59:17 -0400

    First Commit

---
+++ src/studentlists23tueam/Student.java
@@ -0,0 +1,33 @@
+
+package studentlists23tueam;
+
+
+public class Student {
+    private String sname;
+    private String sID;
+
+    public Student(String sID,String sname ) {
+        this.sname = sname;
+        this.sID = sID;
+    }
+
+    
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getsID() {
+        return sID;
+    }
+
+    public void setsID(String sID) {
+        this.sID = sID;
+    }
+    
+    
+    
+}
---
+++ src/studentlists23tueam/StudentListS23TUEam.java
@@ -0,0 +1,27 @@
+
+package studentlists23tueam;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ronak
+ */
+public class StudentListS23TUEam {
+
+    
+    public static void main(String[] args) {
+        Student s1 =new Student("s1","Ronak");
+       // Scanner input = new Scanner(System.in);
+        Student[] studentList =new Student[3];
+        studentList[0]=s1;
+        studentList[1]=new Student("s2","abc");
+        studentList[2]=new Student("s3","def");
+        
+        for(int i =0;i<studentList.length;i++){
+            System.out.println(studentList[i].getSname());
+        }
+        
+    }
+    
+}
